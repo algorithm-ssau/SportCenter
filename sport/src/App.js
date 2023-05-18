789efa87f98c1ecd0e06018f66a3a942ca04828e
KristiKisti <kristinamihaylova19@gmail.com>
2023-05-18 23:19:14 +0500

    jscode

--- sport/src/App.js
+++ sport/src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import photo from './photos/IMG_0251.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() { 
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+      <br/>
+      <div style={{ textAlign: "center",fontFamily: "Montserrat", fontWeight: "bold"}}>Наши Тренеры</div>
+      <br/>
+
+        <div className="container">
+          <div className="box">
+            <div className="imgBox">
+            <img src={photo} className="imgBox" alt="logo" /> 
+            </div>
+            <div className="content">
+              <h2>
+                <span style={{fontSize: "medium"}}>Тренер </span> <br/>
+                <span style={{fontSize: "small"}}> Его описание </span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="box">
+            <div className="imgBox">
+            <img src={photo} className="imgBox" alt="logo" /> 
+            </div>
+            <div className="content">
+              <h2>
+                <span style={{fontSize: "medium"}}>Тренер </span> <br/>
+                <span style={{fontSize: "small"}}> Его описание </span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="box">
+            <div className="imgBox">
+            <img src={photo} className="imgBox" alt="logo" /> 
+            </div>
+            <div className="content">
+              <h2>
+                <span style={{fontSize: "medium"}}>Тренер </span> <br/>
+                <span style={{fontSize: "small"}}> Его описание </span>
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="box">
+            <div className="imgBox">
+            <img src={photo} className="imgBox" alt="logo" /> 
+            </div>
+            <div className="content">
+              <h2>
+                <span style={{fontSize: "medium"}}>Тренер </span> <br/>
+                <span style={{fontSize: "small"}}> Его описание </span>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </header> 
+    </div> 
+  ); 
 }
+
+export default App;
 
 export default App;
