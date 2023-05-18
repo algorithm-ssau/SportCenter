b64734c992d7b99165cec40a1c0cb69441154700
KristiKisti <kristinamihaylova19@gmail.com>
2023-05-18 23:25:29 +0500

    tr

--- sport/src/index.js
+++ sport/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Trainers';
+import Trainers from './Trainers';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
