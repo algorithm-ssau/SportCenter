3ff115973074dcc894c3132fdca7b6868de22af4
adnliss <adeninalissa@mail.ru>
2023-05-28 11:51:24 +0400

    import photos

--- my-app/src/PricesPage.js
+++ my-app/src/PricesPage.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import card1 from './photos/card1.jpg';
+import card2 from './photos/card2.jpg';
+import card3 from './photos/card3.jpg';
+import card4 from './photos/card4.jpg';
+import card5 from './photos/card5.jpg';
+import card6 from './photos/card6.jpg';
 import './Trainers.css';
 
 function PricesPage() {
