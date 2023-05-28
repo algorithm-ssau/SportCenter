d963c2ef651127d966c4ca31cc49c5113a7680d4
ValeriyaAR <romanovaleral@mail.ru>
2023-05-28 21:38:22 +0400

    редактирование файла categ.js

--- my-app/src/Categ.js
+++ my-app/src/Categ.js
@@ -28,85 +28,81 @@ function Categ() {
                     <div style={{ textAlign: "center",fontFamily: "Garamond", fontWeight: "bold",fontSize: "60px"}}>Фитнес-программы и направления</div>
                 </div>
                 
-
                 <div className="p3">
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        
-                        
-                        <Link to="/napravlenie1"><img src={photo1} alt=""/>
-                        <figcaption>
-                            <h3>Групповые <span>программы</span></h3>
-                            <p>В клубе представлено более 30 направлений групповых программ: высокоинтенсивные тренировки, функциональные тренировки, йога, стретчинг</p>
-                            
-                         </figcaption></Link>
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/napravlenie1"><img src={photo1} alt=""/>
+                                <figcaption>
+                                    <h3>Групповые <span>программы</span></h3>
+                                    <p>В клубе представлено более 30 направлений групповых программ: высокоинтенсивные тренировки, функциональные тренировки, йога, стретчинг</p>
+                                </figcaption></Link>
 
                       
-                    </figure>
-                </div>
-                </div>
+                            </figure>
+                        </div>
+                    </div>
+                    
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/Direct2"><img src={photo2} alt=""/>
+                                <figcaption>
+                                    <h3>Персональные <span>тренировки</span></h3>
+                                    <p>Выбор тренера, составление индивидуальной программы, учитывающей все пожелания и многое другое</p>
+                                </figcaption></Link>
+                            </figure>
+                        </div>
+                    </div>
 
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        <Link to="/Direct2"><img src={photo2} alt=""/>
-                        <figcaption>
-                            <h3>Персональные <span>тренировки</span></h3>
-                            <p>Выбор тренера, составление индивидуальной программы, учитывающей все пожелания и многое другое</p>
-                        </figcaption></Link>
-                    </figure>
-                </div>
-                </div>
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/Direct3"><img src={photo3} alt=""/>
+                                <figcaption>
+                                    <h3>Тренажерный <span>зал</span></h3>
+                                    <p>Тренажерный зал находится на двух этажах, огромный выбор блочных и нагружаемых тренажеров на все группы мышц, зона свободных весов, кардио зона</p>
+                                </figcaption></Link>
+                            </figure>
+                        </div>
+                    </div>
 
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        <Link to="/Direct3"><img src={photo3} alt=""/>
-                        <figcaption>
-                            <h3>Тренажерный <span>зал</span></h3>
-                            <p>Тренажерный зал находится на двух этажах, огромный выбор блочных и нагружаемых тренажеров на все группы мышц, зона свободных весов, кардио зона</p>
-                        </figcaption></Link>
-                    </figure>
-                </div>
-                </div>
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/Direct4"><img src={photo4} alt=""/>
+                                <figcaption>
+                                    <h3>Бассейн <span>с морской водой</span></h3>
+                                    <p>Бассейн с морской водой, 25 метров. Детский бассейн. Аквааэробика, классическое плавание, реабилитационные курсы</p>
+                                </figcaption></Link>
+                            </figure>
+                        </div>
+                    </div>
 
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        <Link to="/Direct4"><img src={photo4} alt=""/>
-                        <figcaption>
-                            <h3>Бассейн <span>с морской водой</span></h3>
-                            <p>Бассейн с морской водой, 25 метров. Детский бассейн. Аквааэробика, классическое плавание, реабилитационные курсы</p>
-                        </figcaption></Link>
-                    </figure>
-                </div>
-                </div>
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/Direct5"><img src={photo5} alt=""/>
+                                <figcaption>
+                                    <h3>Йо<span>га</span></h3>
+                                    <p>Позволяет бороться со стрессовыми состояниями, повышает гибкость позвоночника, учит управлять дыханием, способствует оздоровлению и омоложению организма</p>
+                                </figcaption></Link>
+                            </figure>
+                        </div>
+                    </div>
 
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        <Link to="/Direct5"><img src={photo5} alt=""/>
-                        <figcaption>
-                            <h3>Йо<span>га</span></h3>
-                            <p>Позволяет бороться со стрессовыми состояниями, повышает гибкость позвоночника, учит управлять дыханием, способствует оздоровлению и омоложению организма</p>
-                        </figcaption></Link>
-                    </figure>
-                </div>
-                </div>
-
-                <div className="imgblock">
-                <div className="hover-text-one">
-                    <figure className="effect-text-two">
-                        <Link to="/Direct6"><img src={photo6} alt=""/>
-                        <figcaption>
-                            <h3>Стретчинг <span>и шпагат</span></h3>
-                            <p>Развитие гибкости и стройности, ускрепление мышц тела, приобретение уверенности в себе</p>
-                        </figcaption></Link>
-                    </figure>
-                </div>
-                </div>
-            </div>    
+                    <div className="imgblock">
+                        <div className="hover-text-one">
+                            <figure className="effect-text-two">
+                                <Link to="/Direct6"><img src={photo6} alt=""/>
+                                <figcaption>
+                                    <h3>Стретчинг <span>и шпагат</span></h3>
+                                    <p>Развитие гибкости и стройности, ускрепление мышц тела, приобретение уверенности в себе</p>
+                                </figcaption></Link>
+                            </figure>
+                        </div>
+                    </div>
+                </div>    
                 
                 
             </header>
